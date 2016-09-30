11c4f9fa47ae0879c5c86136b2433c31c8f9956f
rtfuzz <Fuzz@Afonsos-MacBook-Pro.local>
2016-09-30 20:19:58 -0300

    teste
    teste

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://easygo.com.br/app/mobileapp/ap",
+	'DialogDefaultTitle' : "EasyGO Mobile",
+	'pushNotificationSenderid' : "664383538550",
+	'facebookAppId' : "213633375702109",
+	'APIHasKey' : "24Donkey11Dick13Stub10Sucker"
+};
