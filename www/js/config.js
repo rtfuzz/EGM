7178c05cec1fe82d2d2b8d475aa05fd2e84de2c9
rtfuzz <Fuzz@Afonsos-MacBook-Pro.local>
2016-09-30 21:17:27 -0300

    error

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 
 var krms_config ={	
-	'ApiUrl' : "http://easygo.com.br/app/mobileapp/ap",
+	'ApiUrl' : "http://easygo.com.br/app/mobileapp/api",
 	'DialogDefaultTitle' : "EasyGO Mobile",
 	'pushNotificationSenderid' : "664383538550",
 	'facebookAppId' : "213633375702109",
